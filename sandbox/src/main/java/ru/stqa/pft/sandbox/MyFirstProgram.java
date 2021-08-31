69a936a04fc6ab77a7814fd17ea0b23ec03e8f0e
Margarita Naumova <margarita.naumova@kofax.com>
2021-08-31 11:32:22 +0300

    Added some functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,29 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Rita");
+
+    double l = 5;
+    System.out.println("S square side " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("S rectangle sides " + a + " and " + b + " = " + area(a, b));
+
   }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
 }
