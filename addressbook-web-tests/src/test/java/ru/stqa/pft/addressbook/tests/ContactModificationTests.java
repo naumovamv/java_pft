7334a1c85817ee5b6e8147dc46e0d242e3446f46
Margarita Naumova <margarita.naumova@kofax.com>
2021-09-10 17:09:46 +0300

    Create ContactModificationTests.java

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void testContactModification() {
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().selectContact();
+    app.getContactHelper().editContact();
+    app.getContactHelper().fillContactForm(new ContactData("Test", "Test", "Saint-Petersburg", "+71111111111", "test@test.ru"));
+    app.getContactHelper().submitContactModification();
+    app.getContactHelper().returnToContactPage();
+
+  }
+
+}
