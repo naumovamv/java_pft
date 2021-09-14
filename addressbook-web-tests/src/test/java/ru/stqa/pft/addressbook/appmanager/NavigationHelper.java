d2b6c3e0a6d4081a187de3deb80d304a9c28d22a
Margarita Naumova <margarita.naumova@kofax.com>
2021-09-14 20:46:28 +0300

    Optimized goto... methods

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -5,17 +5,23 @@ import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase {
 
-  WebDriver wd;
-
   public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
   public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+     return;
+    }
     click(By.linkText("home"));
   }
 }
