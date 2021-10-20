645af72bcabe7083eed5d8b7f106f2bc4a8bbdf8
Margarita Naumova <margarita.naumova@kofax.com>
2021-10-20 17:13:48 +0300

    8.3 and 8.4

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -2,9 +2,10 @@ package ru.stqa.pft.mantis.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.subethamail.smtp.server.Session;
 
 import java.io.File;
 import java.io.FileReader;
@@ -12,36 +13,56 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+
 public class ApplicationManager {
+
   private final Properties properties;
-  WebDriver wd;
-
+  private WebDriver wd;
   private String browser;
-
-
-  public ApplicationManager(String browser) {
+  private RegistrationHelper registrationHelper;
+ 
+  public ApplicationManager(String browser)  {
     this.browser = browser;
     properties = new Properties();
-
   }
+
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.EDGE)){
-      wd = new EdgeDriver();
-    }
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null){
+      wd.quit();
+    }
+  }
+
+  public HttpSession newSession(){ return new HttpSession(this);}
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null){
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null){
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.EDGE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
   }
 }
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegistrationHelper {
+  private final ApplicationManager app;
+  private final WebDriver wd;
+
+  public RegistrationHelper(ApplicationManager app) {
+    this.app = app;
+    wd = app.getDriver();
+  }
+
+ public void start (String usernmae, String email) {
+ wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+ }
+
+}
