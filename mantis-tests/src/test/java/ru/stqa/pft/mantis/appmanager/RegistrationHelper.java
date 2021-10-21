871cf3efe28cf0281de6a01da1e291ab4a7922ab
Margarita Naumova <margarita.naumova@kofax.com>
2021-10-21 17:30:22 +0300

    8.6 Email

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,18 +1,28 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper {
-  private final ApplicationManager app;
-  private final WebDriver wd;
+public class RegistrationHelper extends HelperBase {
 
-  public RegistrationHelper(ApplicationManager app) {
-    this.app = app;
-    wd = app.getDriver();
+  public RegistrationHelper(ApplicationManager app)  {
+    super(app);
+   }
+
+  public void start(String username, String email) {
+    wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+    type(By.name("username"), username);
+    type(By.name("email"), email);
+    click(By.cssSelector("input[value = 'Signup']"));
   }
 
- public void start (String usernmae, String email) {
- wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
- }
+  public void finish(String confirmationLink, String user, String password) {
+    wd.get(confirmationLink);
+    type(By.name("realname"), user);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"), password);
+    //click(By.cssSelector("span.submit-button button"));
+    click(By.cssSelector("input[value='Update User']"));
 
+  }
 }
